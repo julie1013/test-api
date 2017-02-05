364902d929bfad131f889865647d630132636c07
julie1013 <julie1380@verizon.net>
2017-02-04 22:40:37 -0500

    Updated read model
    Updated read model relationship so that it indicates that it's
    inverse of reads for both user and book

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class Read < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :book
+  belongs_to :user, inverse_of: :reads
+  belongs_to :book, inverse_of: :reads
 end
