f080a41d9c93872a3c7ed2f448d1309813dd65f3
julie1013 <julie1380@verizon.net>
2017-02-04 22:23:29 -0500

    Tested CURL scripts.
    Tested CURL requests for GET, POST, UPDATE, DELETE. All work

--- scripts/books.sh
+++ scripts/books.sh
@@ -4,14 +4,16 @@ curl --include --request POST http://localhost:4741/books \
   --header "Content-Type: application/json" \
   --data '{
     "book": {
-      "title": "The Hunger Games",
+      "title": "Mockingjay",
       "author": "Suzanne Collins"
     }
   }'
 
-curl --include --request DELETE https://localhost:4741/books/3
+curl --include --request GET http://localhost:4741/books/
 
-curl --include --request PATCH http://localhost:4741/books/3 \
+curl --include --request DELETE http://localhost:4741/books/3
+
+curl --include --request PATCH http://localhost:4741/books/6 \
   --header "Content-Type: application/json" \
   --data '{
     "book": {
