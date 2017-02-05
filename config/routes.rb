65caa290d7a4e79bd995d3cc477b5a4dd29efbe7
julie1013 <julie1380@verizon.net>
2017-02-04 20:41:29 -0500

    Consolidated routes
    Consolidated routes:
    
    resources: :books, except: [:new, :edit]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  resources :examples, except: [:new, :edit]
+
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
-  get '/books', to: 'books#index'
-  post '/books', to: 'books#create'
-  get '/books/:id', to: 'books#show'
-  patch '/books/:id', to: 'books#update'
-  delete '/books/:id', to: 'books#destroy'
+  resources :books, except: [:new, :edit]
   resources :users, only: [:index, :show]
 end
