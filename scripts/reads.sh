57463c8e5129803aa47e58dc69a1506dd4d8303d
julie1013 <julie1380@verizon.net>
2017-02-04 22:57:04 -0500

    Tested patch request on reads table
    Tested patch request on reads table; changed the rating of
    an entry.

--- scripts/reads.sh
+++ scripts/reads.sh
@@ -9,3 +9,13 @@ curl --include --request POST http://localhost:4741/reads \
   }'
 
   curl --include --request DELETE http://localhost:4741/reads/1
+
+  curl --include --request PATCH http://localhost:4741/reads/2 \
+    --header "Content-Type: application/json" \
+    --data '{
+      "read": {
+        "user_id": "1",
+        "book_id": "4",
+        "rating": "4"
+      }
+    }'
