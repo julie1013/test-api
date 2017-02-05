fb33121e1b694391a97d42b6c275a41b142c4f22
julie1013 <julie1380@verizon.net>
2017-02-04 20:58:11 -0500

    Wrote methods for CRUD actions
    Wrote all methods for CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
