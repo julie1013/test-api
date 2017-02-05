c47ec830041043700211850cffdfd86f2539645a
julie1013 <julie1380@verizon.net>
2017-02-04 22:51:24 -0500

    Created user account, updated serializers, tested join table
    Created a user account with curl. Updated serializers for book and user.
    Added an entry to reads via curl.

---
+++ scripts/reads.sh
@@ -0,0 +1,9 @@
+curl --include --request POST http://localhost:4741/reads \
+  --header "Content-Type: application/json" \
+  --data '{
+    "read": {
+      "user_id": "1",
+      "book_id": "4",
+      "rating": "5"
+    }
+  }'
--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/sign-up"
-curl "${API}${URL_PATH}" \
-  --include \
-  --request POST \
+curl --include --request POST http://localhost:4741/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD}"'"
+      "email": "dog@dog.com",
+      "password": "dog",
+      "password_confirmation": "dog"
     }
   }'
 
